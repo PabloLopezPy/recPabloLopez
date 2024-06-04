4f394f1feaf2ab21cb825038dc204c7a47136f4a
PabloLopezPy <158159397+PabloLopezPy@users.noreply.github.com>
2024-06-04 13:01:15 +0200

    Separada a nueva clase empleados

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,39 +1,18 @@
-import java.util.Scanner;
-
 public class SistemaGestionEmpleados {
-
-    private Empleado[] empleados;
-
-    public SistemaGestionEmpleados(Empleado[] empleados) {
-        this.empleados = empleados;
-    }
-
-    public void aumentarSalario(double porcentaje) {
-        for (Empleado empleado : empleados) {
-            double nuevoSalario = empleado.salario * (1 + porcentaje / 100);
-            empleado.salario = nuevoSalario;
-        }
-    }
-
     public static void main(String[] args) {
-        Empleado[] empleados = new Empleado[3];
-        empleados[0] = new Empleado("Juan", "Desarrollador", 50000);
-        empleados[1] = new Empleado("María", "Diseñadora", 45000);
-        empleados[2] = new Empleado("Pedro", "Gerente", 60000);
+        Empleados ListaEmpleados = new Empleados();
 
-        SistemaGestionEmpleados sistema = new SistemaGestionEmpleados(empleados);
+        Empleado emp1 = new Empleado("Paco", 20, 22000.0);
+        Empleado emp2 = new Empleado("Julen", 25, 2500.0);
+        Empleado emp3 = new Empleado("Pablo", 40, 3000.0);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduzca el porcentaje de aumento de salario: ");
-        double porcentaje = scanner.nextDouble();
+        ListaEmpleados.agregarEmpleado(emp1);
+        ListaEmpleados.agregarEmpleado(emp2);
+        ListaEmpleados.agregarEmpleado(emp3);
 
-        sistema.aumentarSalario(porcentaje);
-        
-        System.out.println("Lista de Empleados:");
-        for (Empleado empleado : empleados) {
-            System.out.println(empleado);
-        }
-
-        scanner.close();
+        ListaEmpleados.mostrarEmpleados();
+        ListaEmpleados.aumentarSalario(10);
+        ListaEmpleados.mostrarEmpleados();
     }
 }
+
