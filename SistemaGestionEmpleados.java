7a8f76ab0c71f1a6dfa45f794e51e2cb9d8b5602
PabloLopezPy <158159397+PabloLopezPy@users.noreply.github.com>
2024-06-04 13:14:38 +0200

    Separada a empleados, esto es puro main

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,17 +1,20 @@
 public class SistemaGestionEmpleados {
     public static void main(String[] args) {
+        // Creas un arryList de clase Empleados
         Empleados ListaEmpleados = new Empleados();
-
+        // Agregas datos ficticios
         Empleado emp1 = new Empleado("Paco", 20, 22000.0);
         Empleado emp2 = new Empleado("Julen", 25, 2500.0);
         Empleado emp3 = new Empleado("Pablo", 40, 3000.0);
-
+        // Los introduces  en el array llamando a agregarEmpleado
         ListaEmpleados.agregarEmpleado(emp1);
         ListaEmpleados.agregarEmpleado(emp2);
         ListaEmpleados.agregarEmpleado(emp3);
-
+        // Muestras los empleados
         ListaEmpleados.mostrarEmpleados();
+        // Haces el aumento 
         ListaEmpleados.aumentarSalario(10);
+        // Vuelves a mostrarlos para ver los cambios actualizados
         ListaEmpleados.mostrarEmpleados();
     }
 }
