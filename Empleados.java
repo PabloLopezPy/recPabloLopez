4d133447ff5b1fb02a0f7ef657274416236e4574
PabloLopezPy <158159397+PabloLopezPy@users.noreply.github.com>
2024-06-04 13:13:45 +0200

    Separamos la logica empleados a una nueva clase

--- Empleados.java
+++ Empleados.java
@@ -1,26 +1,27 @@
 import java.util.ArrayList;
 
 public class Empleados {
+    // Generamos arrayList de la clase empleado
     private ArrayList<Empleado> lista;
-
+    // Creamos sus constructores
     public Empleados() {
         this.lista = new ArrayList<>();
     }
-
+    // Realizamos un metodo para mostrar todos los empleados llamando a Textos y Empleado
     public void mostrarEmpleados() {
         for (Empleado e : lista) {
             System.out.println(Textos.NOMBRE + e.getNombre() + ", " + Textos.EDAD + e.getEdad() + ", " + Textos.SALARIO + e.getSalario());
         }
     }
     
-
+    // Aumentamos el salario, recordar usar double para almacenar datos, sino no se puede
     public void aumentarSalario(double porcentaje) {
         for (Empleado e : lista) {
             double nuevoSalario = e.getSalario() * (1 + porcentaje / 100);
             e.setSalario(nuevoSalario);
         }
     }
-
+    // Agregamos a los empleados a la lista
     public void agregarEmpleado(Empleado empleado) {
         lista.add(empleado);
     }
