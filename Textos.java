44736528881197122717a1c5bc2115f14f652e43
PabloLopezPy <158159397+PabloLopezPy@users.noreply.github.com>
2024-06-04 13:15:15 +0200

    Creada para separar los textos

---
+++ Textos.java
@@ -0,0 +1,6 @@
+public class Textos {
+    // Generamos una clase para tener datos por determinado
+    public static final String NOMBRE = "Nombre: ";
+    public static final String EDAD = "Edad: ";
+    public static final String SALARIO = "Salario: ";
+}
